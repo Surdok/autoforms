251a392abeeb2c3001fd2bd9385f0080693af289
Rich Lowe <rich.e.lowe@ge.com>
2019-02-04 16:58:05 -0600

    Restricted width of add button

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Autoforms v2.0.15
+# Autoforms v2.0.16
 
 Automatic form generation for basic table management.
 
--- views/add.js
+++ views/add.js
@@ -164,8 +164,8 @@ module.exports = (autoform) => {
         /** Otherwise, if the property type is 'array' and input type is 'checkboxes'... */
         else if ( property.type() == `boolean` ) {
           form.radios().colsBefore(property.inputColumnsBefore()).cols(property.inputColumns()).colsAfter(property.inputColumnsAfter()).name(property.name()).label(property.inputLabel()).required(property.required()).disabled(property.disabled()).align(property.alignment());
-          form.option().value(1).text(`Yes`);
-          form.option().value(0).text(`No`).selected(true);
+          form.option().value(1).text(`Yes`).selected(true);
+          form.option().value(0).text(`No`);
         }
       });
       
